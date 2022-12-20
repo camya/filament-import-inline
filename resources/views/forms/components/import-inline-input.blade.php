03792d887eb29b56294372e4c470c92471a026f8
Andreas Scheibel <contact@camya.com>
2022-12-20 14:02:06 +0100

    Commented out :hint

--- resources/views/forms/components/import-inline-input.blade.php
+++ resources/views/forms/components/import-inline-input.blade.php
@@ -4,10 +4,10 @@
     :label="$getLabel()"
     :label-sr-only="$isLabelHidden()"
     :helper-text="$getHelperText()"
-    :hint="$getHint()"
-    :hint-action="$getHintAction()"
-    :hint-color="$getHintColor()"
-    :hint-icon="$getHintIcon()"
+{{--    :hint="$getHint()"--}}
+{{--    :hint-action="$getHintAction()"--}}
+{{--    :hint-color="$getHintColor()"--}}
+{{--    :hint-icon="$getHintIcon()"--}}
     :required="$isRequired()"
     :state-path="$getStatePath()"
     class="filament-paste-input-wrapper"
